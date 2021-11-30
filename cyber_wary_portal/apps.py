7761138eb0cead36c4c0855aa5b76a7cc7c758cf
William P <metallicgloss@gmail.com>
2021-11-30 22:32:06 +0000

    Initial Project Configuration.

---
+++ cyber_wary_portal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CyberWaryPortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cyber_wary_portal'
---
+++ cyber_wary_public/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CyberWaryPublicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cyber_wary_public'
